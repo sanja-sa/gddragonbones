6fabccf31831b64caba32f22c36c1ddea3de61a5
Ranoller <davarrcal@hotmail.com>
2018-08-22 02:07:18 +0200

    Adapt to splitted math_2d.h header file in godot 3.1

--- gddragonbones/src/GDTextureData.h
+++ gddragonbones/src/GDTextureData.h
@@ -3,8 +3,18 @@
 
 #include <memory>
 #include <dragonBones/DragonBonesHeaders.h>
-#include "core/math/math_2d.h"
 #include "scene/resources/texture.h"
+#include "core/version.h"
+
+#if (VERSION_MAJOR == 3 && VERSION_MINOR >= 1)
+#include <core/math/transform_2d.h>
+#include <core/math/vector2.h>
+#include <core/math/rect2.h>
+
+#else
+#include "core/math/math_2d.h"
+
+#endif
 
 DRAGONBONES_NAMESPACE_BEGIN
 
