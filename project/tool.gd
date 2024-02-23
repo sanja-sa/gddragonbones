390f0a593c7fc1e161b6dc9d28e407034dfc5771
Daylily-Zeleen <735170336@qq.com>
2024-02-24 00:12:14 +0800

    移除测试项目

--- project/tool.gd
+++
@@ -1,8 +0,0 @@
-@tool
-extends EditorScript
-
-func _run() -> void:
-	var res = ResourceLoader.load("res://assets/dragonbones_assets/AncientAutomaton_ske.json", "GDDragonBonesResource")
-	print(res, " = ")
-	#print("res://icon.svg".get_basename())
-
